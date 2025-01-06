97c672e104b5c52a523dbd42baf3eb58d12ee111
pandashreyan <pandashreyan7@gmail.com>
2025-01-07 00:46:23 +0530

    Your descriptive message for the third commit

--- src/components/Scene3D.jsx
+++ src/components/Scene3D.jsx
@@ -5,11 +5,7 @@ const Scene3D = () => {
   return (
     <Canvas>
       {/* Basic rotating box */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} />
-      <Box args={[1, 1, 1]} position={[0, 0, 0]}>
-        <meshStandardMaterial attach="material" color="orange" />
-      </Box>
+     
     </Canvas>
   );
 };
